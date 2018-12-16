494f445b621a64d285019b270752d5ec7bc2fe6c
Ryan Palo <ryan@thepalos.com>
2018-12-15 17:44:27 -0800

    Refactor day 13

--- python/src/day13.py
+++ python/src/day13.py
@@ -28,10 +28,46 @@ class Vector:
     def __hash__(self):
         return hash((self.x, self.y))
 
+
 NORTH = Vector(0, -1)
 SOUTH = Vector(0, 1)
 EAST = Vector(1, 0)
 WEST = Vector(-1, 0)
+DIRECTIONS = [NORTH, EAST, SOUTH, WEST]
+
+
+class Cart:
+    """A cart that drives around the mine"""
+
+    def __init__(self, position: Vector, direction: Vector):
+        self.position = position
+        self.direction = direction
+        self.next_turn = "left"
+
+    def move(self):
+        """Updates position based on direction"""
+        self.position += self.direction
+
+    def turn_right(self):
+        self.direction = DIRECTIONS[
+            (DIRECTIONS.index(self.direction) + 1) % len(DIRECTIONS)
+        ]
+
+    def turn_left(self):
+        self.direction = DIRECTIONS[
+            (DIRECTIONS.index(self.direction) - 1) % len(DIRECTIONS)
+        ]
+
+    def handle_next_turn(self):
+        """Update own direction based on the next turn value specified in the prompt"""
+        if self.next_turn == "left":
+            self.turn_left()
+            self.next_turn = "straight"
+        elif self.next_turn == "straight":
+            self.next_turn = "right"
+        elif self.next_turn == "right":
+            self.turn_right()
+            self.next_turn = "left"
 
 
 class Mine:
@@ -58,108 +94,76 @@ class Mine:
                 else:
                     self.map[-1].append(c)
 
-    def find_first_collision(self):
+    def find_first_collision(self) -> Vector:
         """Simulates the mine until two carts collide.  Returns that x, y pair"""
         while True:
-            self.carts.sort(key=lambda cart: (cart.position.y, cart.position.x))
+            self.carts.sort(key=lambda cart: (
+                cart.position.y, cart.position.x))
 
             for cart in self.carts:
                 cart.move()
+                self.turn_cart(cart)
 
-                if sum(other.position == cart.position for other in self.carts) > 1:
+                if self.is_collision(cart):
                     return cart.position
-                # print(cart.position)
-                new_location = self.map[cart.position.y][cart.position.x]
-                if new_location == '+':
-                    cart.handle_next_turn()
-                elif new_location == '/':
-                    if cart.direction == NORTH or cart.direction == SOUTH:
-                        cart.turn_right()
-                    else:
-                        cart.turn_left()
-                elif new_location == '\\':
-                    if cart.direction == NORTH or cart.direction == SOUTH:
-                        cart.turn_left()
-                    else:
-                        cart.turn_right()
 
-    def last_cart_location(self):
+    def last_cart_location(self) -> Vector:
         """Simulates the mine, removing carts when they collide.
         Returns the x, y pair of the last cart standing
         """
         while True:
-            self.carts.sort(key=lambda cart: (
-                cart.position.y, cart.position.x))
+            self.sort_carts()
+
             crashed = set()
             for cart in self.carts:
                 if cart.position in crashed:
                     continue
                 cart.move()
+                self.turn_cart(cart)
 
-                if sum(other.position == cart.position for other in self.carts) > 1:
+                if self.is_collision(cart):
                     crashed.add(cart.position)
                     continue
-                # print(cart.position)
-                new_location = self.map[cart.position.y][cart.position.x]
-                if new_location == '+':
-                    cart.handle_next_turn()
-                elif new_location == '/':
-                    if cart.direction == NORTH or cart.direction == SOUTH:
-                        cart.turn_right()
-                    else:
-                        cart.turn_left()
-                elif new_location == '\\':
-                    if cart.direction == NORTH or cart.direction == SOUTH:
-                        cart.turn_left()
-                    else:
-                        cart.turn_right()
-            self.carts = [cart for cart in self.carts if cart.position not in crashed]
+
+            self.carts = [cart
+                          for cart in self.carts
+                          if cart.position not in crashed]
             if len(self.carts) == 1:
                 return self.carts[0].position
 
+    def sort_carts(self):
+        """Carts are always run in reading order, top to bottom, left to right"""
+        self.carts.sort(key=lambda cart: (cart.position.y, cart.position.x))
 
-class Cart:
-    """A cart that drives around the mine"""
-    DIRECTIONS = [Vector(1, 0), Vector(0, 1), Vector(-1, 0), Vector(0, -1)]
+    def is_collision(self, cart: Cart) -> bool:
+        """Checks whether or not a cart is running into any other carts"""
+        return sum(other.position == cart.position for other in self.carts) > 1
 
-    def __init__(self, position: Vector, direction: Vector):
-        self.position = position
-        self.direction = direction
-        self.next_turn = "left"
+    def turn_cart(self, cart: Cart):
+        """Depending on what kind of track the cart is on, turn it appropriately"""
+        track_type = self.map[cart.position.y][cart.position.x]
+        if track_type == '+':
+            cart.handle_next_turn()
+        elif track_type == '/':
+            if cart.direction == NORTH or cart.direction == SOUTH:
+                cart.turn_right()
+            else:
+                cart.turn_left()
+        elif track_type == '\\':
+            if cart.direction == NORTH or cart.direction == SOUTH:
+                cart.turn_left()
+            else:
+                cart.turn_right()
 
-    def move(self):
-        """Updates position based on direction"""
-        self.position += self.direction
-
-    def turn_right(self):
-        self.direction = Cart.DIRECTIONS[
-            (Cart.DIRECTIONS.index(self.direction) + 1) % len(Cart.DIRECTIONS)
-        ]
-
-    def turn_left(self):
-        self.direction = Cart.DIRECTIONS[
-            (Cart.DIRECTIONS.index(self.direction) - 1) % len(Cart.DIRECTIONS)
-        ]
-
-    def handle_next_turn(self):
-        """Update own direction based on the next turn value specified in the prompt"""
-        if self.next_turn == "left":
-            self.turn_left()
-            self.next_turn = "straight"
-        elif self.next_turn == "straight":
-            self.next_turn = "right"
-        elif self.next_turn == "right":
-            self.turn_right()
-            self.next_turn = "left"
 
 if __name__ == "__main__":
     with open("python/data/day13.txt", "r") as f:
         mine_map = f.read()
-    
+
     # Part 1
     mine = Mine(mine_map)
     print(mine.find_first_collision())
-    
+
     # Part 2
     mine = Mine(mine_map)
     print(mine.last_cart_location())
