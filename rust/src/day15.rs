464a7a60227f834707b9e177d97fcc35738b1080
Ryan Palo <ryan@thepalos.com>
2019-04-18 14:59:31 -0700

    [WIP] Day 15: Get attack selection working.

--- rust/src/day15.rs
+++ rust/src/day15.rs
@@ -15,6 +15,7 @@ pub struct Position {
     x: usize,
     y: usize,
     value: char,
+    health: usize,
 }
 
 type Grid = Vec<Vec<Position>>;
@@ -69,6 +70,16 @@ pub fn best_next_step(map: Grid, start: Position, target: Position) -> Position 
     neighbors[index]
 }
 
+pub fn select_attack_target(map: Grid, attacker: Position) -> Position {
+    let neighbors: Vec<Position> = vec![
+        map[attacker.y-1][attacker.x],
+        map[attacker.y][attacker.x-1],
+        map[attacker.y][attacker.x+1],
+        map[attacker.y+1][attacker.x],
+    ];
+    neighbors.into_iter().enumerate().min_by_key(|(i, pos)| (pos.health, *i)).unwrap().1
+}
+
 pub fn distance(start: Position, end: Position) -> usize {
     ((end.x as isize - start.x as isize).abs() + (end.y as isize - start.y as isize).abs()) as usize
 }
@@ -78,7 +89,7 @@ pub fn parse_map(text: &str) -> Vec<Vec<Position>> {
     for (y, row) in text.lines().enumerate() {
         result.push(Vec::new());
         for (x, c) in row.chars().enumerate() {
-            result[y].push(Position{x: x, y: y, value: c});
+            result[y].push(Position{x: x, y: y, value: c, health: 200});
         }
     }
     result
@@ -99,22 +110,22 @@ mod tests {
         assert_eq!(
             vec![
                 vec![
-                    Position{x: 0, y: 0, value: '#'},
-                    Position{x: 1, y: 0, value: '#'},
-                    Position{x: 2, y: 0, value: '#'},
-                    Position{x: 3, y: 0, value: '#'},
+                    Position{x: 0, y: 0, value: '#', health: 200},
+                    Position{x: 1, y: 0, value: '#', health: 200},
+                    Position{x: 2, y: 0, value: '#', health: 200},
+                    Position{x: 3, y: 0, value: '#', health: 200},
                 ],
                 vec![
-                    Position{x: 0, y: 1, value: '#'},
-                    Position{x: 1, y: 1, value: '.'},
-                    Position{x: 2, y: 1, value: 'E'},
-                    Position{x: 3, y: 1, value: '#'},
+                    Position{x: 0, y: 1, value: '#', health: 200},
+                    Position{x: 1, y: 1, value: '.', health: 200},
+                    Position{x: 2, y: 1, value: 'E', health: 200},
+                    Position{x: 3, y: 1, value: '#', health: 200},
                 ],
                 vec![
-                    Position{x: 0, y: 2, value: '#'},
-                    Position{x: 1, y: 2, value: '#'},
-                    Position{x: 2, y: 2, value: '#'},
-                    Position{x: 3, y: 2, value: '#'},
+                    Position{x: 0, y: 2, value: '#', health: 200},
+                    Position{x: 1, y: 2, value: '#', health: 200},
+                    Position{x: 2, y: 2, value: '#', health: 200},
+                    Position{x: 3, y: 2, value: '#', health: 200},
                 ],
             ], parse_map(text)
         );
@@ -130,14 +141,14 @@ mod tests {
 #######
 ".trim();
         let map = parse_map(&text);
-        let start = Position{x: 1, y: 1, value: 'E'};
+        let start = Position{x: 1, y: 1, value: 'E', health: 200};
         assert_eq!(
-            Some(Position{x: 3, y: 1, value: '.'}),
+            Some(Position{x: 3, y: 1, value: '.', health: 200}),
             find_nearest_reachable(map, start)
         );
     }
 
-        #[test]
+    #[test]
     fn test_choose_best_next_step() {
         let text = "
 #######
@@ -147,11 +158,30 @@ mod tests {
 #######
 ".trim();
         let map = parse_map(&text);
-        let start = Position{x: 2, y: 1, value: 'E'};
-        let target = Position{x: 4, y: 2, value: '.'};
+        let start = Position{x: 2, y: 1, value: 'E', health: 200};
+        let target = Position{x: 4, y: 2, value: '.', health: 200};
         assert_eq!(
-            Position{x: 3, y: 1, value: '.'},
+            Position{x: 3, y: 1, value: '.', health: 200},
             best_next_step(map, start, target)
         );
+    }
+
+    #[test]
+    fn test_attack_weakest_reading() {
+        let text = "
+#######
+#G....#
+#..G..#
+#..EG.#
+#..G..#
+#######
+".trim();
+        let mut map = parse_map(&text);
+        map[2][3] = Position{x: 3, y: 2, value: 'G', health: 4};
+        let expected = Position{x: 4, y: 3, value: 'G', health: 2};
+        map[3][4] = expected;
+        map[4][3] = Position{x: 3, y: 4, value: 'G', health: 2};
+        let target: Position = select_attack_target(map, Position{x: 3, y: 3, value: 'E', health: 200});
+        assert_eq!(target, expected);
     }
 }
